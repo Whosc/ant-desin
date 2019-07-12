eb557027c84ff40cdaee5ec5c4d5ca3b67fe2488
Whosc <1421851519@qq.com>
2019-07-12 18:41:53 +0800

    初始化配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
+import Antd from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import "ant-design-vue/dist/antd.less";
+// import "ant-design-vue/lib/button/style";
+// import {Button} from "ant-design-vue";
 Vue.config.productionTip = false;
 
+Vue.use(Antd);
+
 new Vue({
+  Antd,
   router,
   store,
   render: h => h(App)
